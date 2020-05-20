1b171c25118a5586aa819ecd429667670ccfe2bf
John Teske <john.teske@protonmail.com>
2020-05-19 20:43:07 -0700

    Check statuses of lines of files

--- src/cli/stream-file.ts
+++ src/cli/stream-file.ts
@@ -23,18 +23,15 @@ async function getFile(readable: Readable): Promise<string> {
 }
 
 export async function getOne(files: Filename[]): Promise<LinkStatus[]> {
-  const links: string[] = await Promise.all(
+  const links: string[][] = await Promise.all(
     files.map(
-      async (f): Promise<string> => {
+      async (f): Promise<string[]> => {
         const stream = read(f);
         const file = await getFile(stream);
-        return file;
-        //return file.split(/\n/).filter(Boolean);
+        return file.split(/\n/).filter(Boolean);
       }
     )
-  ); //.then((ll) => ll.flat());
-  const links2 = [].concat(...links);
-  console.log(links2);
-  return getAllStatuses([]);
-  //return getAllStatuses(links);
+  );
+
+  return getAllStatuses([].concat(...links));
 }
